97e9669c69c5bd762007ecfc98bacaee92ca782b
Dmitriy Erlih <derlih@gmail.com>
2011-11-04 09:48:00 +0400

    create QApplication in debug and QCoreApplication in release build

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
+#if defined(QT_GUI_LIB)
+    #include <QApplication>
+#else
+    #include <QCoreApplication>
+#endif
+
+#include <memory>
+
 int main(int argc, char **argv)
 {
-	return 0;
+    std::auto_ptr<QCoreApplication> app(
+            #if defined(QT_GUI_LIB)
+                new QApplication(argc, argv)
+            #else
+                new QCoreApplication(argc, argv)
+            #endif
+                );
+
+
+
+
+    return app->exec();
 }
--- qtupdater.pro
+++ qtupdater.pro
@@ -1,4 +1,7 @@
 TEMPLATE = app
 CONFIG  += qt warn_on release
+QT = core
+
+CONFIG(debug):QT += gui
 
 SOURCES += main.cpp
