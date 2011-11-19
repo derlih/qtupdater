cc569b38e63bbfe35b9b4699910850346e8eda59
Dmitriy Erlih <derlih@gmail.com>
2011-11-19 23:16:05 +0400

    js console object changed

--- scripthelpers/scriptconsole.cpp
+++ scripthelpers/scriptconsole.cpp
@@ -2,22 +2,43 @@
 
 #include <QDebug>
 
+//===========================================================================//
+
+static void printToConsole(QtMsgType type, QString msg);
+
+//===========================================================================//
+
 ScriptConsole::ScriptConsole(QObject *parent)
     : QObject(parent)
 {
+
 }
 
 void ScriptConsole::log(QString msg)
 {
-    qDebug() << msg;
+    printToConsole(QtDebugMsg, msg);
 }
 
 void ScriptConsole::warn(QString msg)
 {
-    qWarning() << msg;
+    printToConsole(QtWarningMsg, msg);
 }
 
 void ScriptConsole::critical(QString msg)
 {
-    qCritical() << msg;
+    printToConsole(QtCriticalMsg, msg);
+}
+
+//===========================================================================//
+
+void printToConsole(QtMsgType type, QString msg)
+{
+    QDebug d = qDebug();
+
+    if(type == QtWarningMsg)
+        d = qWarning();
+    else
+        d = qCritical();
+
+    d << "[JS]" << msg;
 }
